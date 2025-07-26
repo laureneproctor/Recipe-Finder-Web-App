a25e271e9c6105c37165f475e1e69bdda7071f2c
laureneproctor <laurenproctor464@gmail.com>
2025-07-25 21:19:57 -0400

    Connected backend to local MongoDB, created Recipe model, and configured Mongoose in server.js

--- README.md
+++ README.md
@@ -3,5 +3,10 @@
 Members and Roles:
 Lauren Proctor - frontend and backend development
 
+## How to Run
+1. Open MongoDB and press Connect for localhost:27017
+2. Open terminal and cd into backend directory, run ```npm run dev```
+3. Open another terminal window and cd into fronted directory, run ```npm start```
+
 ## Iteration 1
 Implemented the basic structure of project, notably the routing, a navbar via bootstrap, basic API setup via Express, and connected frontend and backend w/ Axios.
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 const cors = require('cors');
+const mongoose = require('mongoose');
+
 app.use(cors());
-
-
 app.use(express.json());
 
-// Add this:
+// mongoDB connection
+const mongoURI = 'mongodb://localhost:27017/recipe_finder';
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+// routing
 const recipeRoutes = require('./api/recipes');
 app.use('/api/recipes', recipeRoutes);
 
@@ -18,4 +29,3 @@ app.get('/api/health', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
